e01a2b3005e9afcc191d9a2ed862c4957d48c4d2
Alex Nelson <sergeantsalt@gmail.com>
2020-03-30 22:12:40 -0400

    raw input and ouput complete

--- kilo.c
+++ kilo.c
@@ -31,6 +31,11 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
+  PAGE_UP,
+  PAGE_DOWN,
 };
 
 // terminal
@@ -97,11 +102,33 @@ int parseEscapeSeq() {
   if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
   if (seq[0] == '[') {
+    if (seq[1] >= '0' && seq[1] <= '9') {
+      if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+      if (seq[2] == '~') {
+        switch (seq[1]) {
+          case '1': return HOME_KEY;
+          case '3': return DEL_KEY;
+          case '4': return END_KEY;
+          case '5': return PAGE_UP;
+          case '6': return PAGE_DOWN;
+          case '7': return HOME_KEY;
+          case '8': return END_KEY;
+        }
+      }
+    } else {
+      switch (seq[1]) {
+        case 'A': return ARROW_UP;
+        case 'B': return ARROW_DOWN;
+        case 'C': return ARROW_RIGHT;
+        case 'D': return ARROW_LEFT;
+        case 'H': return HOME_KEY;
+        case 'F': return END_KEY;
+      }
+    }
+  } else if (seq[0] == 'O') {
     switch (seq[1]) {
-      case 'A': return ARROW_UP;
-      case 'B': return ARROW_DOWN;
-      case 'C': return ARROW_RIGHT;
-      case 'D': return ARROW_LEFT;
+      case 'H': return HOME_KEY;
+      case 'F': return END_KEY;
     }
   }
 
@@ -223,16 +250,24 @@ void abFree(struct abuf *ab) {
 void editorMoveCursor(int key) {
   switch (key) {
     case ARROW_LEFT:
-      E.cx--;
+      if (E.cx != 0) {
+        E.cx--;
+      }
       break;
     case ARROW_RIGHT:
-      E.cx++;
+      if (E.cx != E.screenCols - 1) {
+        E.cx++;
+      }
       break;
     case ARROW_UP:
-      E.cy--;
+      if (E.cy != 0) {
+        E.cy--;
+      }
       break;
     case ARROW_DOWN:
-      E.cy++;
+      if (E.cy != E.screenRows - 1) {
+        E.cy++;
+      }
       break;
   }
 }
@@ -249,6 +284,23 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+
+    case HOME_KEY:
+      E.cx = 0;
+      break;
+
+    case END_KEY:
+      E.cx = E.screenCols - 1;
+      break;
+
+    case PAGE_UP:
+    case PAGE_DOWN:
+      {
+        int times = E.screenRows;
+        while (times--) {
+          editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+        }
+      }
 
     case ARROW_UP:
     case ARROW_DOWN:
